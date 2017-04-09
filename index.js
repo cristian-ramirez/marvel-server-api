30c31ccd6e6d43a878671594db3422117f2324a4
Cristian Ramirez <cristian.ramirez@gorillalogic.com>
2017-04-08 21:53:20 -0600

    test heroku

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api', router);
+// app.use('/api', router);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
 
 app.listen(7000, () => {
 	console.log('Server running on port 7000');
